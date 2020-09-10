2c62bed4e7e6abc5af654ff25b5ca6b4b4923ea8
Justinas <j.zailskas@eif.viko.lt>
2020-09-10 21:53:32 +0300

    View komponentu isdestymas

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default class App extends Component {
+  render() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ flex: 1, backgroundColor: '#FFF55A'}}>
+        <Text>25 procentai</Text>
+      </View>
+      <View style={{flex: 1, backgroundColor: '#57D563'}}>
+        <Text>50 procentu</Text>
+      </View>
+      <View style={{flex: 1, flexDirection: 'row', backgroundColor: '#FF3232'}}>
+        <View style={{flex: 1, borderWidth: 2}}>
+
+        </View>
+        <View style={{flex:1, borderWidth: 2}}>
+
+        </View>
+      </View>
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
