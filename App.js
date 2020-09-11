f3386e31894129de49b0f1651e7e9b328552b28b
Justinas <j.zailskas@eif.viko.lt>
2020-09-11 08:28:09 +0300

    Paskutiniai patepimai

--- App.js
+++ App.js
@@ -1,31 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
-const Button = () => (
+/*const Buttonn = () => (
   <View style={styles.buttonStyle}>
     <TouchableOpacity>
       <Text>Spausti</Text>
     </TouchableOpacity>
   </View>
-)
+)*/
 
 export default class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      fontWeight : '100',
+      reiksme: 0
+    }
+  }
+  onClick() {
+    const fontWeights = [
+      "normal",
+      "bold",
+      "100",
+      "200",
+      "300",
+      "400",
+      "500",
+      "600",
+      "700",
+      "800",
+      "900"
+    ];
+    
+    let randomFontWeight = fontWeights[Math.floor(Math.random()*fontWeights.length)]
+    console.log(randomFontWeight)
+    this.setState({
+      fontWeight: randomFontWeight
+    })
+  }
+  changeNumber(){
+    var i = 0
+    var newReiksme = 0
+    while (i<100){
+      newReiksme = this.state.reiksme + 1
+      i++
+    }
+    this.setState({
+      reiksme: newReiksme,
+    })
+  }
   render() {
   return (
     <View style={styles.container}>
       <View style={{ flex: 1, backgroundColor: '#FFF88A'}}>
-        <Text>25 procentai</Text>
+        <Text style={{fontWeight: this.state.fontWeight}}>33 procentai</Text>
       </View>
       <View style={{flex: 1, backgroundColor: '#21D296'}}>
-        <Text>50 procentu</Text>
+        <Text>{this.state.reiksme}</Text>
       </View>
       <View style={{flex: 1, flexDirection: 'row', backgroundColor: '#CFF635'}}>
         <View style={styles.containerAndBorder}>
-          <Button/>
+          <Button onPress={()=> this.onClick()}
+            title = "Change Font Weight"
+          />
         </View>
         <View style={styles.containerAndBorder}>
-          <Button/>
+          <Button 
+            onPress={()=> this.changeNumber()}
+            title="Push"
+          />
         </View>
       </View>
     </View>
@@ -48,6 +92,6 @@ const styles = StyleSheet.create({
   },
   buttonStyle: {
     borderWidth: 1,
-    backgroundColor: '#DDDDDD'
-  }
+    backgroundColor: '#DDDDDD',
+  }, 
 });
